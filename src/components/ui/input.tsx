8116cc589d0f5c7ffe1a10aed8393fbdd54b76aa
violan7naidoo <violan7naidoo@gmail.com>
2024-04-30 22:50:13 +0200

    src/lib/utils

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -3,11 +3,9 @@ import * as React from "react";
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input(
-  { ...props },
-  ref
-) {
-  return <input {...props} ref={ref} />;
+const Input = React.forwardRef<HTMLInputElement, InputProps>((props, ref) => {
+  const { className, type, ...rest } = props; // Destructuring props to access className and type
+  return <input type={type} {...rest} ref={ref} />;
 });
 Input.displayName = "Input";
 
