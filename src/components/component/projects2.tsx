06fe39bec0a42ff011061523a2f6258b2abe944f
violan7naidoo <violan7naidoo@gmail.com>
2024-05-01 10:00:06 +0200

    demo

--- src/components/component/projects2.tsx
+++ src/components/component/projects2.tsx
@@ -38,7 +38,7 @@ export default function projects2() {
             </p>
             <Link
               className="inline-flex justify-centre h-8 items-center rounded-md border border-gray-200  bg-white px-4 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-              href="https://sensory-wonderland.vercel.app/"
+              href="https://next-js-ecommerce-wheat.vercel.app/"
               target="_blank"
             >
               View Project
